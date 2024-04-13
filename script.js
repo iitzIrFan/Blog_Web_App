ec0572bb59d4fd0863091938ea9fa072e0429652
iitzIrFan <irfanulhawk@gmail.com>
2024-04-13 18:47:49 +0530

    Web app convention...

--- script.js
+++ script.js
@@ -156,3 +156,12 @@ const openBlog = (event) => {
   const getBlog = globalStore.find(blog => blog.id === targetID);
   blogModal.innerHTML = htmlModalContent(getBlog);
 };
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
