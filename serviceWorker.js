b1bd4b74618225202afc662b0049440367fcce8b
iitzIrFan <irfanulhawk@gmail.com>
2024-04-13 20:09:20 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,41 +1,24 @@
-const staticCacheName = "blog-static-v1";
-
+const staticDevCoffee = "dev-coffee-site-v1"
 const assets = [
   "/",
   "/index.html",
   "/css/style.css",
   "/js/app.js",
   "logo.png",
-];
+]
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(staticCacheName).then(cache => {
-      return cache.addAll(assets);
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
     })
-  );
-});
+  )
+})
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-
-      return fetch(event.request).then(networkResponse => {
-        if (!networkResponse || networkResponse.status !== 200) {
-          return networkResponse;
-        }
-
-        const responseToCache = networkResponse.clone();
-
-        caches.open(staticCacheName).then(cache => {
-          cache.put(event.request, responseToCache);
-        });
-
-        return networkResponse;
-      });
-    })
-  );
-});
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
