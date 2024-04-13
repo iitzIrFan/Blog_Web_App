0ee299c82878a7a04cd045b4ae08cf5c8db59fc8
iitzIrFan <irfanulhawk@gmail.com>
2024-04-13 19:57:55 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,5 @@
-const staticDevCoffee = "dev-coffee-site-v1";
+const staticCacheName = "blog-static-v1";
+
 const assets = [
   "/",
   "/index.html",
@@ -9,7 +10,7 @@ const assets = [
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
+    caches.open(staticCacheName).then(cache => {
       cache.addAll(assets);
     })
   );
