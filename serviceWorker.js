a450a45e7e821d085bf3625d5b5e4e47cd2128f0
iitzIrFan <irfanulhawk@gmail.com>
2024-05-11 13:21:36 +0530

    Added offline capability to web app by setting cache and assets to be cached...

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,7 @@
+// Define const variable cachevar and set its value to My Assets
 const cachevar = "MY Assets"
+
+// Arrays of assets to be cached
 const assets = [
   "/",
   "/index.html",
@@ -7,18 +10,27 @@ const assets = [
   "/logo.png",
 ]
 
+// install event
 self.addEventListener("install", installEvent => {
+  // When the "install" event occurs, wait until the following tasks are completed
   installEvent.waitUntil(
+    // Open cache named My Assets
     caches.open(cachevar).then(cache => {
+      // Add assets to cache
       cache.addAll(assets)
     })
   )
 })
 
+// fech event
 self.addEventListener("fetch", fetchEvent => {
+  // When a fetch event occurs, respond to it with the following logic
     fetchEvent.respondWith(
+      // Find match in the cache
       caches.match(fetchEvent.request).then(res => {
-        return res || fetch(fetchEvent.request)
+        // If a match is found, return the cached response
+        // If no match is found, fetch the resource from the network
+        return res || fetch(fetchEvent.request) // or opearator
       })
     )
   })
